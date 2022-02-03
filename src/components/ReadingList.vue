3771d3ccfa0e3ca5a89b9171bd04b3435027b526
oskarskold <Oskar.skold4@hotmail.com>
2022-02-03 13:58:57 +0100

    Added vuex

--- src/components/ReadingList.vue
+++ src/components/ReadingList.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
+  <div class="readingList">
     <h2>{{ book.Title }}</h2>
     <p>{{ book.Author }}</p>
-    <button>remove</button>
+    <button @click="removeFromReadingList">remove</button>
   </div>
 </template>
 
 <script>
 export default {
   props: ["book"],
+  methods: {
+    removeFromReadingList() {
+      this.$store.commit("removeFromReadingList", this.book);
+    },
+  },
+  computed: {
+    books() {
+      return this.$store.state.books;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.readingList {
+  background: #2f2f2f;
+}
+.readingList > h2,
+p {
+  color: white;
+}
+</style>
